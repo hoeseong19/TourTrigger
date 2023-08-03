7eb404e70d8dbbe546cd86da19cea2ac9be87604
hoeseong <hoeseong19@naver.com>
2023-08-03 11:19:08 +0900

    StatusCodes 설치 및 test status code 예측에 사용

--- tests/integrations/index.e2e.test.js
+++ tests/integrations/index.e2e.test.js
@@ -1,9 +1,10 @@
+const HTTPStatusCode = require("http-status-codes");
 const request = require("supertest");
 const app = require("../../app");
 
 describe("Index", () => {
     // 첫 페이지
     it("GET /", (done) => {
-        request(app).get("/").expect(200, done);
+        request(app).get("/").expect(HTTPStatusCode.OK, done);
     });
 });
